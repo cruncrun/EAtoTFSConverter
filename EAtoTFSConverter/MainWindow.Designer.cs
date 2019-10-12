d7398d96992e60bfd1bb015ebc59575d3b00a290
Kamil Otrebscki <crunchipsccc@gmail.com>
2019-10-12 18:04:51 +0200

    Add project files.

---
+++ EAtoTFSConverter/MainWindow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EAtoTFSConverter
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_load_EA_XML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_load_EA_XML
+            // 
+            this.btn_load_EA_XML.Location = new System.Drawing.Point(12, 12);
+            this.btn_load_EA_XML.Name = "btn_load_EA_XML";
+            this.btn_load_EA_XML.Size = new System.Drawing.Size(154, 23);
+            this.btn_load_EA_XML.TabIndex = 0;
+            this.btn_load_EA_XML.Text = "Wczytaj XML z EA";
+            this.btn_load_EA_XML.UseVisualStyleBackColor = true;
+            this.btn_load_EA_XML.Click += new System.EventHandler(this.Btn_load_EA_XML_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 641);
+            this.Controls.Add(this.btn_load_EA_XML);
+            this.Name = "MainWindow";
+            this.Text = "EAtoTFSConverter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_load_EA_XML;
+    }
+}
+
---
+++ EAtoTFSConverter/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace EAtoTFSConverter
+{
+    public partial class MainWindow : Form
+    {        
+        private string filePath = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_load_EA_XML_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "C:\\",
+                Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {                
+                filePath = openFileDialog.FileName;
+                XDocument source = XDocument.Load(filePath);
+            }
+        }
+    }
+}
